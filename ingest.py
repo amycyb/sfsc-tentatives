8e0ed6594fc70583df83e060cb7a46fc43f6e83e
Amy Chattopadhyay <amitabhochattopadhyay@gmail.com>
2026-04-27 14:51:03 -0700

    Create ingest.py

---
+++ ingest.py
@@ -0,0 +1,6 @@
+git clone https://github.com/amitabho/sfsc-tentatives.git
+cd sfsc-tentatives
+# create ingest.py and .gitignore here (paste the content)
+git add ingest.py .gitignore
+git commit -m "Add ingest.py and .gitignore"
+git push
